40cabf1dd166741c6fbe7629ed62ba75c1a17f94
hardjopranoto <38484602+hardjopranoto@users.noreply.github.com>
2019-12-15 21:10:18 +1100

    Create variables.tf

---
+++ deploy_webserver/variables.tf
@@ -0,0 +1,22 @@
+# Defining the variables for admin_username and admin_password for the VM
+# You may change this to your own admin username and password
+# Resource prefix name will be prompted during run time
+
+variable "admin_username" {
+  type = string
+  default = "localadmin"
+}
+
+variable "admin_password" {
+  type = string
+  default = "C0mpl3xPa55w0rd!"
+}
+
+variable "resource_prefix" {
+  type = string
+}
+
+# You'll usually want to set this to a region near you.
+variable "location" {
+  default = "australiaeast"
+}
