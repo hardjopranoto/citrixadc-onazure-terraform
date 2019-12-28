dccdb94618945a29af4636a1ffa11c1769beb37b
Daniel <38484602+hardjopranoto@users.noreply.github.com>
2019-12-29 07:28:50 +1100

    Update main.tf

--- vnet_peer/main.tf
+++ vnet_peer/main.tf
@@ -1,6 +1,5 @@
 resource "null_resource" "vnet_peer" {
   provisioner "local-exec" {
-  command = "az network vnet peering create"
-  interpreter = ["az","--name peer-tfdemo-to-tfnsdemo","--resource-group tfdemo_rg","--vnet-name tfdemo-vnet","--remote-vnet tfnsdemo-vnet","--allow-vnet-access","--subscription eebeec37-91ca-43f9-a46b-1cd65dfcc8e7"]
+  command = "az network vnet peering create --name peertfdemototfnsdemo --resource-group tfdemo_rg --vnet-name tfdemo-vnet --remote-vnet tfnsdemo-vnet --allow-vnet-access --subscription eebeec37-91ca-43f9-a46b-1cd65dfcc8e7"
   }
 }        
