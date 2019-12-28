7a4f8e55d4504e40797a10ae834250558ea36aa9
Daniel <38484602+hardjopranoto@users.noreply.github.com>
2019-12-28 21:14:17 +1100

    .

--- simple_lb/variables.tf
+++ simple_lb/variables.tf
@@ -1,17 +1,3 @@
-vip_config = {
-  vip = "10.10.1.5"
-}
-
-backend_service_config = {
-  clttimeout   = 40
-  backend_port = 80
-}
-
-backend_services = [
-  "10.11.1.4",
-  "10.11.1.5",
-]
-
 variable "location" {
   default = "australiaeast"
 }
