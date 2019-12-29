f71c33a4c05bbd4b07ccd29a1d6c6ec55977d269
Daniel <38484602+hardjopranoto@users.noreply.github.com>
2019-12-29 19:35:58 +1100

    .

--- simple_lb/main.tf
+++ simple_lb/main.tf
@@ -1,6 +1,28 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
 provider "citrixadc" {
   username = "ctxadmin"
   password = "CtxPa55w0rd!"
-  endpoint = "http://52.156.166.49/"
+  endpoint = "http://13.73.203.23/"
 }
 
+resource "citrixadc_lbvserver" "production_lb" {
+  name        = "productionLB"
+  ipv46       = var.vip_config["vip"]
+  port        = "80"
+  servicetype = "HTTP"
+}
+
+resource "citrixadc_servicegroup" "backend" {
+  servicegroupname = "productionBackend"
+  lbvservers       = [citrixadc_lbvserver.production_lb.name]
+  servicetype      = "HTTP"
+  clttimeout       = var.backend_service_config["clttimeout"]
+  servicegroupmembers = formatlist(
+    "%s:%s",
+    var.backend_services,
+    var.backend_service_config["backend_port"],
+  )
+}
